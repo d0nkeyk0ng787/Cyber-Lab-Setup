3590dc7accf818c9e94e50432666d2dedaed1a21
Neb <77905135+d0nkeyk0ng787@users.noreply.github.com>
2022-10-05 14:03:38 +1100

    Update installfsvr1.ps1

--- MakeADEnv/4FSVR1/installfsvr1.ps1
+++ MakeADEnv/4FSVR1/installfsvr1.ps1
@@ -3,31 +3,40 @@
 # Disable IPv6
 Disable-NetAdapterBinding -Name 'Ethernet' -ComponentID 'ms_tcpip6' | Out-Null
 
+Write-Host "IPv6 Disabled" -ForegroundColor Cyan
+
+# Resuable Variables
+$IP = "192.168.100.5"
+$Gateway = "192.168.100.1"
+$DC = "192.168.100.2"
+
 # Assign a static IP
 $ipparams = @{
     InterfaceIndex = (Get-NetAdapter).InterfaceIndex
-    IPAddress = "192.168.100.5"
+    IPAddress = $IP
 	PrefixLength = 24
-	DefaultGateway = "192.168.100.1"
+	DefaultGateway = $Gateway
 }
 
 New-NetIPAddress @ipparams | Out-Null
 
 # Completed message
-Write-Host "Server has been assigned an IP of 192.168.100.5" -ForegroundColor Cyan
-Write-Host "Server has been assigned a Default Gateway of 192.168.100.1" -ForegroundColor Cyan
+Write-Host "Server has been assigned an IP of $IP" -ForegroundColor Cyan
+Write-Host "Server has been assigned a Default Gateway of $Gateway" -ForegroundColor Cyan
 
 # Change the DNS server to DC1 
-Set-DnsClientServerAddress -InterfaceAlias (Get-NetAdapter).name -ServerAddresses 192.168.100.2 | Out-Null
+Set-DnsClientServerAddress -InterfaceAlias (Get-NetAdapter).name -ServerAddresses $DC | Out-Null
 
 # Completed message
-Write-Host "Servers DNS server has been changed to 192.168.100.2" -ForegroundColor Cyan
+Write-Host "Servers DNS server has been changed to $DC" -ForegroundColor Cyan
 
 # Create a domain credential object
 $Password = ConvertTo-SecureString "Password1" -AsPlainText -Force
 $DomainCred = New-Object System.Management.Automation.PSCredential ("xyz\Administrator", $Password) 
 
 Start-Sleep -Seconds 5
+
+Write-Host "Renaming server to FSVR1 and joining server to the domain" -ForegroundColor Cyan
 
 # Join computer to the domain
 $domainparams = @{
@@ -39,4 +48,4 @@ $domainparams = @{
 	Restart = $true
 }
 
-Add-Computer @domainparams
+Add-Computer @domainparams | Out-Null
