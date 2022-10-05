da91af95949259ee41002eabe5ef64681937d454
Neb <77905135+d0nkeyk0ng787@users.noreply.github.com>
2022-10-05 14:04:39 +1100

    Update installcli.ps1

--- MakeADEnv/5CLI1/installcli.ps1
+++ MakeADEnv/5CLI1/installcli.ps1
@@ -3,14 +3,30 @@
 # Disable IPv6
 Disable-NetAdapterBinding -Name 'Ethernet' -ComponentID 'ms_tcpip6' | Out-Null
 
-# Set the DNS
-Set-DNSClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses 192.168.100.2
+Write-Host "IPv6 Disabled" -ForegroundColor Cyan
 
-Write-Host "Clients DNS server was changed to 192.168.100.2" -ForegroundColor Cyan
+# Resuable Variables
+$DC = "192.168.100.2"
+
+# Set the DNS
+Set-DNSClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $DC
+
+Write-Host "Clients DNS server was changed to $DC" -ForegroundColor Cyan
 
 # Create a domain credential object
 $Password = ConvertTo-SecureString "Password1" -AsPlainText -Force
 $DomainCred = New-Object System.Management.Automation.PSCredential ("xyz\Administrator", $Password) 
 
-# Join to domain
-Add-Computer -DomainName xyz.local -Credential $DomainCred -Force -Restart
+Write-Host "Joining client to the domain" -ForegroundColor Cyan
+
+# Join computer to the domain
+$domainparams = @{
+	DomainName = "xyz.local"
+	NewName = "CLI1"
+	OUPath = "OU=Workstations,OU=Devices,OU=XYZ,DC=xyz,DC=local"
+	Credential = $DomainCred
+	Force = $true
+	Restart = $true
+}
+
+Add-Computer @domainparams | Out-Null
